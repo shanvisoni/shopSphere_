e40b6f330091e0b4ab01a266e3297f6b9a2fa24e
shanvisoni <shanvisoni7388@gmail.com>
2024-12-25 00:07:02 +0530

    commands updates

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.use('/api/v1/product',productRoutes);
 //     res.send({msg:"welcome to ecommerce app"})
 // })
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, './client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, './client/build/index.html'));
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
   }
 
